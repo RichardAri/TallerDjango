986a72096aacaabd33467c5276c9fa402bc64216
RichardAri <richardaripari@gmail.com>
2024-04-09 19:49:32 -0500

    Add App1

--- Contenedor/TallerDj/settings.py
+++ Contenedor/TallerDj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #apps propias
+    'App1.apps.App1Config'
 ]
 
 MIDDLEWARE = [
